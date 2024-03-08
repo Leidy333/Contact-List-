e558958f252ba4693bec991154a88e845b1cef66
leidy <leidy@DESKTOP-452UI5F>
2024-03-07 16:29:06 -0800

    creating table

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function ContactList(){
+    return(
+        <table>
+            <thread>
+                <tr>
+                    <th>Contact List</th>
+                </tr>
+            </thread>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
